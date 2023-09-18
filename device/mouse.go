bfaa94b8552109db5af61e3a333c6e989ac6971a
kuangjian <j.kuang@comsand.com>
2023-09-18 10:56:38 +0800

    Move

--- device/mouse.go
+++ device/mouse.go
@@ -3,5 +3,14 @@ package device
 import "github.com/go-vgo/robotgo"
 
 func TouchMove(x int, y int) {
-	robotgo.Move(x, y)
+
+	robotgo.MoveRelative(x, y)
+
+	//fmt.Println("MoveLocation:1:", x, y)
+	//currentX, currentY := robotgo.Location()
+	//fmt.Println("MoveLocation:2:", currentX, currentY)
+	//offsetX, offsetY := x-currentX, y-currentY
+	//fmt.Println("MoveLocation:3:", offsetX, offsetY)
+	//fmt.Println("MoveLocation:4:", currentX+offsetX, currentY+offsetY)
+	//robotgo.Move(currentX+offsetX, currentY+offsetY)
 }
